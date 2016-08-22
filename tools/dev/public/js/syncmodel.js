5a54a9655d7226a383dc25d595786d115ed6c9f5
Andifeind <andifeind@noname-media.com>
2016-08-22 18:45:04 +0200

    examples

--- tools/dev/public/js/syncmodel.js
+++ tools/dev/public/js/syncmodel.js
@@ -1,5 +1,6 @@
 var SyncModel = new XQCore.SyncModel('example', function(self) {
   self.on('data.change', function(data) {
     console.log('CHANGE', data);
+    document.getElementsByClassName('counter')[0].innerHTML = data.counter;
   })
 });
